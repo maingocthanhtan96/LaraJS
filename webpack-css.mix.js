53d69045ccada99cbdc52976ca98ef9c36740c30
Tan Mai <laudaikinhdi@gmail.com>
2020-02-10 13:32:45 +0700

    prettier update

--- webpack-css.mix.js
+++ webpack-css.mix.js
@@ -5,45 +5,42 @@ require('laravel-mix-eslint');
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: [
-    './resources/js/**/*.{vue,js}',
-  ],
+  content: ['./resources/js/**/*.{vue,js}'],
   whitelist: [
     'html',
     'body',
     'app',
-    ...whitelister([
-      'node_modules/element-ui/lib/theme-chalk/index.css',
-    ]),
+    ...whitelister(['node_modules/element-ui/lib/theme-chalk/index.css'])
   ],
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
 
 mix.extend('mergeManifest', mergeManifest);
 
 mix.webpackConfig({
   output: {
-    chunkFilename: 'js/chunks/[name].js',
+    chunkFilename: 'js/chunks/[name].js'
   },
   resolve: {
     alias: {
-      '@': path.join(__dirname, '/resources/js'),
-    },
-  },
+      '@': path.join(__dirname, '/resources/js')
+    }
+  }
 });
 
-mix.options({
-  processCssUrls: false,
-  postCss: [
-    require('tailwindcss')('./public/js/tailwind.config.js'),
-    require('autoprefixer'),
-    mix.inProduction() ? purgecss : require('autoprefixer'),
-  ],
-  clearConsole: true, // in watch mode, clears console after every build
-})
+mix
+  .options({
+    processCssUrls: false,
+    postCss: [
+      require('tailwindcss')('./public/js/tailwind.config.js'),
+      require('autoprefixer'),
+      mix.inProduction() ? purgecss : require('autoprefixer')
+    ],
+    clearConsole: true // in watch mode, clears console after every build
+  })
   .sass('resources/js/styles/index.scss', 'public/css/app.css', {
-    implementation: require('node-sass'),
+    implementation: require('node-sass')
   })
   .mergeManifest();
 
@@ -53,7 +50,7 @@ if (mix.inProduction()) {
   if (process.env.LARAJS_USE_ESLINT === 'true') {
     mix.eslint({
       fix: true,
-      cache: false,
+      cache: false
     });
   }
   // Development settings
@@ -61,10 +58,7 @@ if (mix.inProduction()) {
   //   proxy: process.env.APP_URL,
   //   files: ['resources/js/**/*']
   // });
-  mix
-    .sourceMaps()
-    .webpackConfig({
-      devtool: 'cheap-eval-source-map', // Fastest for development
-    });
+  mix.sourceMaps().webpackConfig({
+    devtool: 'cheap-eval-source-map' // Fastest for development
+  });
 }
-
